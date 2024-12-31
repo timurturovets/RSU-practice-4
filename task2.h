be863657f777d285e4c13e1d0082012b0c31bf34
timurturovets <timurturovets@mail.ru>
2024-12-31 07:02:59 +0300

    task 2

--- task2.h
+++ task2.h
@@ -1,7 +1,7 @@
 
 
 typedef struct String {
-    char *inner_array;
+    char *value;
     int len;
 } String, *p_string;
 
@@ -22,7 +22,7 @@ int string_constructor(p_string dest, char *src) {
 
     String entity;
 
-    entity.inner_array = inner_array;
+    entity.value = inner_array;
     entity.len = len;
 
     *dest = entity;
@@ -30,16 +30,142 @@ int string_constructor(p_string dest, char *src) {
     return OK;
 }
 
-int task_2(int argc, char** argv) {
-    char *text = "lorem ipsum";
+int clear_string(p_string str) {
+    if (str == NULL) return INVALID_PARAMETER;
 
-    String str;
-    string_constructor(&str, text);
+    str->len = 0;
+    if (str->value != NULL) free(str->value);
 
-    printf("Str len: %d\n", str.len);
-    printf("Str: ");
-    for (int i = 0; i < str.len; i++) {
-        printf("%c", str.inner_array[i]);
+    return OK;
+}
+
+// otnoshenie poryadka i equivalentnost v odnom...
+int string_cmp(int *result, p_string str_1, p_string str_2) {
+    if (str_1 == NULL || str_2 == NULL) return INVALID_PARAMETER;
+
+    int i, len;
+
+    if (str_1->len != str_2->len) {
+        if (str_1->len > str_2->len) *result = 1;
+        else *result = -1;
+
+        return OK;
     }
 
+    for (i = 0, len = str_1->len; i < len; i++) {
+        if (str_1->value[i] > str_2->value[i]) {
+            *result = 1;
+
+            return OK;
+        } else if (str_1->value[i] < str_2->value[i]) {
+            *result = -1;
+            return OK;
+        }
+    }
+
+    *result = 0;
+    return OK;
+}
+
+int string_cpy(p_string dest, p_string src) {
+    if (dest == NULL || src == NULL) return INVALID_PARAMETER;
+
+    int i, len = src->len;
+
+    clear_string(dest);
+
+    dest->value = (char*) malloc(src->len * sizeof(char));
+    if (dest->value == NULL) return MEMORY_ALLOCATION_ERROR;
+
+    for (i = 0; i < len; i++) {
+        dest->value[i] = src->value[i];
+    }
+
+    dest->len = len;
+
+    return OK;
+}
+
+int make_str_from_source(p_string result, p_string src) {
+    if (src == NULL) return INVALID_PARAMETER;
+
+    int i, len = src->len;
+    String new_str;
+
+    new_str.value = (char*) malloc(len * sizeof(char));
+    if (new_str.value == NULL) return MEMORY_ALLOCATION_ERROR;
+
+    new_str.len = len;
+
+    for (i = 0; i < len ; i++) {
+        new_str.value[i] = src->value[i];
+    }
+
+    *result = new_str;
+
+    return OK;
+}
+
+int string_concat(p_string dest, p_string src) {
+    if (dest == NULL || src == NULL) return INVALID_PARAMETER;
+
+    char* old_buf;
+    int i, new_len = dest->len + src->len;
+
+    old_buf = dest->value;
+    dest->value = (char*) realloc(dest->value, new_len * sizeof(char));
+    if (dest->value == NULL) {
+        free(old_buf);
+
+        return MEMORY_ALLOCATION_ERROR;
+    }
+
+    for (i = dest->len; i < new_len; i++) {
+        dest->value[i] = src->value[i - dest->len];
+    }
+
+    dest->len = new_len;
+
+    return OK;
+}
+
+int task_2(int argc, char** argv) {
+    int i, result;
+
+    char *text_1 = "lorem ipsum",
+         *text_2 = "dolor sit amet",
+         *text_3 = "lorem ipsum",
+         *text_4 = "l1rem ipsum";
+
+    String str_1, str_2, str_3, str_4;
+    string_constructor(&str_1, text_1);
+    string_constructor(&str_2, text_2);
+    string_constructor(&str_3, text_3);
+    string_constructor(&str_4, text_4);
+
+    printf("Str: ");
+    for (i = 0; i < str_1.len; i++) {
+        printf("%c", str_1.value[i]);
+    }
+    printf("\nStr len: %d\n", str_1.len);
+
+    string_cmp(&result, &str_1, &str_2);
+    printf("Comparing result 1: %d\n", result);
+
+    string_cmp(&result, &str_1, &str_3);
+    printf("Comparing result 2: %d\n", result);
+
+    string_cmp(&result, &str_3, &str_4);
+    printf("Comparing result 3: %d\n", result);
+
+    clear_string(&str_2);
+    printf("String len after clearing: %d\n", str_2.len);
+
+    string_concat(&str_1, &str_4);
+    printf("After concat:");
+    for (i = 0; i < str_1.len; i++) {
+        printf("%c", str_1.value[i]);
+    }
+
+    return OK;
 }
